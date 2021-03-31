fd89aa715af3cac94617995a0362b0538d8857f5
Peter Lava Espersen <pede.13@live.dk>
2021-03-31 15:26:55 +0200

    fixed axios calls

--- src/views/Pictures.vue
+++ src/views/Pictures.vue
@@ -1,61 +1,45 @@
-
-
 <template>
   <div>
     <button @click="callApi">Update</button>
-    <button @click="getImages">images</button>
-
     <h1>{{apiMessage}}</h1>
-    <img    :src="`data:image/png;base64,${picture}`" alt="nothing">
-    <div>{{picture}}</div>
+    <img v-if="pictureUrl" :src="pictureUrl[0]" alt="nothing">
+    <img v-if="pictureUrl" :src="pictureUrl[1]" alt="nothing">
+    <img v-if="pictureUrl" :src="pictureUrl[2]" alt="nothing">
+
   </div>
 </template>
+<script lang="ts">
 
-<script>
 import axios from "axios";
+
+//type apiData = {"success": Boolean, "files": [{"id": String, "filename": String }]}
+
 export default {
   name: "pictures",
   components: {
   },
   data() {
     return {
-      apiMessage: {"success": Boolean, "files": [{"id": String, "filename": String }]},
-      picture: {}
+      apiMessage: {"success": Boolean, "files": {"id": String, "filename": String }},
+      pictureUrl: Array
     };
   },
   methods: {
     async callApi() {
-      // Get the access token from the auth wrapper
-      const token = await this.$auth.getTokenSilently();
-
+      let array = [];
       // Use Axios to make a call to the API
-      const { data } = await axios.get("http://localhost:3001/images", {
-        headers: {
-          Authorization: `Bearer ${token}`    // send the access token through the 'Authorization' header
-        }
+   //  type apiData = {"success": Boolean, "files": [{"id": String, "filename": String }]}
+      await axios.get("http://localhost:3001/images").then((fetchData)=>{
+       fetchData.data.files.forEach(function (img){
+          array.push(`http://localhost:3001/image/${img.filename}?tn=${true}`);
+       })
+        this.pictureUrl = array;
+        //this.apiMessage = fetchData.data
       })
-      this.apiMessage = data;
-
-
-
     },
-    async getImages() {
-      const token = await this.$auth.getTokenSilently();
-
-      const { data } = await axios.get("http://localhost:3001/image/"+this.apiMessage.files[0].filename,{
-        responseType: 'arraybuffer',
-        headers: {
-          Authorization: `Bearer ${token}`    // send the access token through the 'Authorization' header
-        }
-      })
-
-      this.picture = Buffer.from(data, "binary").toString('base64');
-    }
-
   }
 };
 </script>
-
 <style lang="scss" scoped>
 .next-steps {
   .fa-link {
